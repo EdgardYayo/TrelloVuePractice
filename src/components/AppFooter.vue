608bb1faa2a6fcce686bf7a8f87b7f1276b672e4
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-07-27 14:53:24 -0700

    Añadiendo principales componentes de la aplicacion de tareas tipo trello con Vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer>
+        <span>💻 with ❤ &amp; <a href="//vuejs.org">Vue.js</a> in Mexico</span>
+    </footer>
+</template>
+
+
+<script>
+export default { 
+  name: 'app-footer'
+}
+</script>
+
+<styles scoped>
+  footer {
+    box-sizing: border-box;
+    background-color: #cfd8dc;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 1rem;
+    width: 100%;
+  }
+
+  a {
+      text-decoration: none;
+      color: #546E7A;
+    }
+
+</styles>
---
+++ src/components/views/Board.vue
@@ -0,0 +1,91 @@
+<template>
+    <section>
+        <h3><span>Mis Paneles</span> 💨 {{ name }}</h3>
+        <input
+          type="text"
+          placeholder="Añade una lista"
+          v-model="listName"
+          @keyup.enter="add()"
+        />
+        <div class="container">
+            <column
+              v-for="(list, index) in boardList"
+              :key="index"
+              :listId="list.id"
+              :name="list.name">
+            </column>
+        </div>
+    </section>
+</template>
+
+
+<script>
+import Column from '@/components/Column'
+
+export default {
+    name: 'board-view',
+    components: { Column },
+    props: {
+        name: String,
+        id: String
+    },
+
+    data () {
+        return {
+            listName: '',
+            boardList: [
+                { id: '1', name: 'Todo'},
+                { id: '2', name: 'Doing'},
+            ]
+        }
+    },
+
+    methods: {
+        add () {
+            this.boardList.push({ name: this.listName })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    section {
+        text-align: left;
+    }
+
+    h3 {
+        color: #37474f;
+        text-align: left;
+        margin: 1.5rem;
+    }
+
+    span {
+        color: #546e7a;
+    }
+
+    
+    input {
+        box-sizing: border-box;
+        background-color: #607d8b;
+        color: #607d8b;
+        border: 0;
+        border-radius: 3px;
+        box-shadow: 0 0 0 0.5px rgba(49,49,93,0.03),
+          0 2px 5px 0 rgba(49, 49, 93, 0.1),
+          0 1px 2px 0 rgba(0, 0, 0, 0.08);
+        font-size: 1.2rem;
+        outline: 0;
+        padding: 1rem;
+        transition: all 600ms ease;
+    }
+
+    input:active, input:focus {
+        background-color: #fafafa
+    }
+
+    input::placeholder {
+        color: white;
+    }
+
+
+</style>
